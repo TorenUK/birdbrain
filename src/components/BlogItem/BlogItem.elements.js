3313a4d082ff8b017bb8bd3b16bc6996755cb136
TorenUK <td_fraser@hotmail.co.uk>
2021-01-05 14:59:44 +0000

    add blog responsiveness

--- src/components/BlogItem/BlogItem.elements.js
+++ src/components/BlogItem/BlogItem.elements.js
@@ -3,12 +3,17 @@ import styled from "styled-components";
 export const BlogContainer = styled.div`
   width: 100%;
   max-width: 1200px;
-  height: 450px;
+
+  min-height: 450px;
   margin: 50px 0;
 
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+
+  @media screen and (max-width: 1000px) {
+    flex-direction: column;
+  }
 `;
 
 export const BlogSection = styled.div`
@@ -21,23 +26,42 @@ export const BlogSection = styled.div`
   align-items: center;
 
   background-color: white;
+
+  @media screen and (max-width: 1000px) {
+    width: 80%;
+
+    text-align: center;
+  }
 `;
 
 export const BlogImage = styled.div`
   width: 100%;
-  height: 100%;
+
+  min-height: 400px;
 
   background-image: url(${(props) => props.imageURL});
   background-position: center;
   background-size: 80%;
   background-repeat: no-repeat;
+
+  @media screen and (max-width: 1000px) {
+    background-size: contain;
+  }
 `;
 
-export const BlogTitle = styled.h2``;
+export const BlogTitle = styled.h2`
+  @media screen and (max-width: 1000px) {
+    margin: 20px 0;
+  }
+`;
 
 export const BlogDescription = styled.p`
   width: 90%;
 
   font-size: 1rem;
   line-height: 22px;
+
+  @media screen and (max-width: 1000px) {
+    width: 100%;
+  }
 `;
