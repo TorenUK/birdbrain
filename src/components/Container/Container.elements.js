ae7d09dae7979fed99e30a1cbc9ab1f57a1253a5
TorenUK <td_fraser@hotmail.co.uk>
2021-01-10 15:52:35 +0000

    resize container

--- src/components/Container/Container.elements.js
+++ src/components/Container/Container.elements.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ContainerWrapper = styled.div`
   width: ${(props) => props.width || "100%"};
   min-height: ${(props) => props.minHeight || "100px"};
-  margin: 80px 0;
+  margin: 100px 0;
 
   display: flex;
   flex-wrap: wrap;
--- src/components/Header/Header.elements.js
+++ src/components/Header/Header.elements.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const HeaderContainer = styled.div`
   width: 100%;
   height: 100px;
-  margin: 50px 0;
+  margin: 30px 0 0 0;
 
   display: flex;
   justify-content: center;
