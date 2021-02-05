2bdcdd8fc17713ab2c032083b46cee016788d1e9
Toren Fraser <toren.fraser@insurancerevolution.co.uk>
2021-02-05 00:01:57 +0000

    init basket function

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,6 +7,35 @@ import { Container, ContactForm } from "../components";
 
 import GlobalStyle from "../globalStyles";
 
+// other
+import { lodash as _ } from "lodash";
+
+const item = {
+  name: "milk",
+  price: 3,
+  qty: 200,
+};
+
+const basket = [{ name: "banana", qty: 1 }];
+
+const add = (_basket, item) => {
+  if (_basket.length) {
+    const regex = new RegExp(item.name);
+
+    return _basket.map((i, idx) => {
+      if (regex.test(i.name)) {
+        _basket[idx].qty += item.qty;
+
+        return _basket;
+      } else {
+        return [..._basket, item];
+      }
+    });
+  }
+};
+
+console.log(add(basket, item));
+
 const Contact = () => {
   return (
     <>
