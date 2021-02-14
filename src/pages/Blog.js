ca43b31c296e8b0ae7846dcd84845fd05cfabb8e
Toren Fraser <toren.fraser@insurancerevolution.co.uk>
2021-02-14 16:29:47 +0000

    loading

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -21,14 +21,17 @@ import { selectBasket } from "../features/basket/basketSlice";
 
 const Blog = () => {
   const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const basket = useSelector(selectBasket);
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get("https://birdbrain.herokuapp.com/blogs")
       .then((response) => {
         setBlogs(response.data);
+        setLoading(false);
       })
       .catch((err) => console.log(err));
   }, []);
@@ -38,7 +41,7 @@ const Blog = () => {
       <GlobalStyle />
       <Navbar />
       <Links />
-      <Header title="Blog" />
+      <Header title={loading ? 'loading blogs...' : 'Blogs'} />
       <PageContainer>
         {blogs.map((blog, idx) => (
           <BlogItem
